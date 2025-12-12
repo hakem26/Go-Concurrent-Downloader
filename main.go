b91a67177aaf26a1a7f4525c2e356b751b174277
hakem26 <hamed26km@gmail.com>
2025-12-12 20:50:13 +0000

    add download single file

--- main.go
+++ main.go
@@ -1,23 +1,53 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+)
+
 func DownloadFile(url, destDir string) error {
 	//آدرس فایل
-
-	//تبدیل به آدرس محلی
-
+	fileName := filepath.Base(url)
+	filePath := filepath.Join(destDir, fileName)
 	//سخت فایل (حتما بسته شه)
+	out, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	fmt.Println("Downloading file ", url)
+	start := time.Now()
 
 	//درخواست HTTP (اینم حتما بسته شه)
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
 
 	//ارور هندلینگ 404 یا 500 که فایل ناقص پاک کنه
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad response %s", res.Status)
+	}
 
 	//ریختن فایل از نت به آدرس محلی
-
+	_, err = io.Copy(out, res.Body)
+	if err != nil {
+		return err
+	}
+	
+	fmt.Printf("Download %s took %s\n", fileName, time.Since(start))
+	return nil
 }
 
 func SequentialDownloader(urls []string, destDir string) error {
 	// حلقه ساده برای دانلود تک تک فایل ها
-
+	return nil
 }
 
 // TODO Result struct
@@ -36,12 +66,19 @@ func ConcurrentDownloader(urls []string, destDir string, maxConcurrent int) erro
 	// وقتی همه تموم شدن، کانال results بسته شود
 
 	// نتایج خوانده و نمایش داده شود
+	return nil
 }
 
 func main() {
 	// ساخت urls جدید
+	url := "https://news-cdn.varzesh3.com/pictures/2025/12/12/D/ng3ktfp25.webp?w=350"
 
 	// اضافه کردن ConcurrentDownloader با ارور هندلینگ
+	err := DownloadFile(url, "./")
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	}
 
 	// لاگ نهایی
+	fmt.Println("Done")
 }
